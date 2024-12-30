10411860ed02d3161698caa630f35a8ea4931eca
Taeho Kim <dittos@gmail.com>
2024-12-31 00:45:02 +0900

    delete unused code

--- tests/translators/test_utils.py
+++
@@ -1,7 +0,0 @@
-from jako.translators.utils import detect_repeated_text
-
-
-def test_detect_repeated_text():
-    assert detect_repeated_text("helloworldhello", 15) is None
-    assert detect_repeated_text("helloworldhello", 6) is None
-    assert detect_repeated_text("helloworldhello", 3) == (0, "hel")
