08f311676e98165da4f88e40f3d6f7685af3921e
Taeho Kim <dittos@gmail.com>
2024-12-31 00:49:07 +0900

    update web

--- src/jako/web/server.py
+++ src/jako/web/server.py
@@ -6,6 +6,9 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+from jako.models.page import PageData
+from jako.preprocess_html import preprocess_split_html
+
 app = FastAPI()
 
 web_dir = Path(__file__).parent
@@ -18,17 +21,9 @@ templates = Jinja2Templates(directory=web_dir / "templates")
 @app.get("/", response_class=HTMLResponse)
 def list_view(request: Request):
     pages = []
-    page_set = set()
     for f in os.listdir(Path("data/result")):
         page = f.replace(".json", "")
         pages.append({"page": page, "complete": True})
-        page_set.add(page)
-
-    for f in os.listdir(Path("data/checkpoint")):
-        page = f.replace(".json", "")
-        if page not in page_set:
-            pages.append({"page": page, "complete": False})
-            page_set.add(page)
 
     return templates.TemplateResponse(
         request=request, name="list.html", context={"pages": pages}
@@ -36,21 +31,40 @@ def list_view(request: Request):
 
 
 @app.get("/result/{page}", response_class=HTMLResponse)
-def result_view(request: Request, page: str, debug: bool = False):
+def result_view(request: Request, page: str):
     result_path = Path("data/result") / f"{page}.json"
-
-    if debug or not result_path.exists():
-        data = json.loads((Path("data/checkpoint") / f"{page}.json").read_text())
-        return templates.TemplateResponse(
-            request=request, name="debug.html", context=data
-        )
-    
     data = json.loads(result_path.read_text())
     return templates.TemplateResponse(
         request=request, name="result.html", context={
             "page": page,
             "title": data["title"],
             "html": data["html"],
+        }
+    )
+
+
+@app.get("/source", response_class=HTMLResponse)
+def source_list_view(request: Request):
+    pages = []
+    for f in os.listdir(Path("data/source")):
+        page = f.replace(".json", "")
+        pages.append({"page": page})
+
+    return templates.TemplateResponse(
+        request=request, name="source_list.html", context={"pages": pages}
+    )
+
+
+@app.get("/source/{page}", response_class=HTMLResponse)
+def source_view(request: Request, page: str, debug: bool = False):
+    source_path = Path("data/source") / f"{page}.json"
+    
+    data = PageData.model_validate_json(source_path.read_text())
+    chunks, _restore_info = preprocess_split_html(data.page.text, title=data.page.title, size=4096)
+    return templates.TemplateResponse(
+        request=request, name="source.html", context={
+            "data": data,
+            "chunks": chunks,
         }
     )
 
