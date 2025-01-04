f3339ebff2b58f74602ad23b0686285374398cd7
Taeho Kim <dittos@gmail.com>
2025-01-04 22:38:35 +0900

    retry

--- src/jako/llm.py
+++ src/jako/llm.py
@@ -1,6 +1,8 @@
+import asyncio
 import os
 from google import genai
 from google.genai import types
+from google.genai.errors import ClientError
 
 from jako.cache import Cache
 
@@ -27,8 +29,29 @@ class GoogleGenaiClient:
                 result["parsed"] = {}  # workaround
             return types.GenerateContentResponse(**result)
         
-        return await cache.wrap("google", self._client.aio.models.generate_content, encode_result, decode_result)(
+        return await cache.wrap("google", self._agenerate_content_with_retry, encode_result, decode_result)(
             model=model,
             contents=contents,
             config=config,
         )
+    
+    async def _agenerate_content_with_retry(
+        self,
+        *,
+        model: str,
+        contents: types.ContentListUnionDict,
+        config: types.GenerateContentConfigOrDict | None = None,
+    ) -> types.GenerateContentResponse:
+        for _ in range(10):
+            try:
+                return await self._client.aio.models.generate_content(
+                    model=model,
+                    contents=contents,
+                    config=config,
+                )
+            except ClientError as e:
+                if e.code == 429:
+                    await asyncio.sleep(10)
+                    continue
+                raise
+        raise Exception("retry failed")
